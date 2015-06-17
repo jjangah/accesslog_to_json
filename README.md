bd9848e35f195f4ba9042db001e701c5c94b9ebb
jjangAh <jungkim2013@gmail.com>
2015-06-17 12:39:33 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # accesslog_to_json
+
+1. Read NginX access log from file. 
+2. Convert to Json format.
+3. Save as file.
+
---
+++ accesslog_to_json.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os, sys
+import re
+import json
+
+# open file : where to read
+f = open("filename", 'r')
+# open file : where to write
+new_f = open("desire_filename", 'w+')
+
+while 1:
+	line = f.readline()
+	if not line:
+		break
+	# get values from pattern
+	m = re.search( r'(.*) -(.*)- \[(.*)\] \"(.*)\" (.*) (.*) \"(.*)\" \"(.*)\" (.*) (.*) .', line)
+
+	dic={}
+	# set eacj access.log attribute and value
+	dic['remote_addr'] = m.group(1)
+	dic['remote_user'] = m.group(2)
+	dic['time_local'] = m.group(3)
+	dic['request'] = m.group(4)
+	dic['status'] =  m.group(5)
+	dic['body_bytes_sent'] = m.group(6)
+	dic['http_referer'] = m.group(7)
+	dic['http_user_agent'] =  m.group(8)
+	dic['request_time'] = m.group(9)
+	dic['upstream_response_time'] = m.group(10)
+
+	new_f.write(json.dumps(dic))
+	new_f.write("\n")
+
+new_f.close()
+f.close()
